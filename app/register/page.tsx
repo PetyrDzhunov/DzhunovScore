a087adeb4adb0a39a36c83afe66dc958ca9da3d4
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2023-10-21 20:27:03 +0300

    clear comments and add zod library for validation

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,14 @@
 type RegisterPageProps = {};
+import { z } from 'zod';
 
 const RegisterPage: React.FC<RegisterPageProps> = ({}) => {
-  return <form>asd</form>;
+  return (
+    <form>
+      <div>
+        <input type='email' />
+      </div>
+    </form>
+  );
 };
 
 export default RegisterPage;
--- components/Button/button-utils.ts
+++ components/Button/button-utils.ts
@@ -2,10 +2,10 @@ import { ButtonSize } from './types';
 
 export const getButtonSize = (buttonSize: ButtonSize): string => {
   if (buttonSize === 'small') {
-    return 'px-2 py-1 w-20 text-sm'; // Example width and height for small size
+    return 'px-2 py-1 w-20 text-sm';
   } else if (buttonSize === 'medium') {
-    return 'px-4 py-2 w-32 text-base'; // Example width and height for medium size
+    return 'px-4 py-2 w-32 text-base';
   } else {
-    return 'px-6 py-3 w-48 text-lg'; // Example width and height for large size
+    return 'px-6 py-3 w-48 text-lg';
   }
 };
