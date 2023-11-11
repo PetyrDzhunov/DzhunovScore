3ef6b5c20b3a2d45ec790534e3781d665d27d0ae
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2023-11-11 12:55:41 +0200

    improve table styling

--- components/CountryTable/CountryTable.tsx
+++ components/CountryTable/CountryTable.tsx
@@ -1,11 +1,6 @@
 'use client';
 import React from 'react';
-import {
-  LeagueById,
-  StandingsItem,
-  StandingsStats,
-  Team,
-} from '@/types/api/api-types';
+import { LeagueById, StandingsItem, Team } from '@/types/api/api-types';
 import Image from 'next/image';
 
 type CountryTableProps = {
@@ -14,10 +9,10 @@ type CountryTableProps = {
 
 const CountryTable: React.FC<CountryTableProps> = ({ footballData }) => {
   return (
-    <div className='overflow-x-auto m-auto bg-black text-white'>
+    <div className='overflow-x-auto mx-auto text-white mt-6'>
       {footballData.map((league) => (
-        <div key={league.id} className='mb-8'>
-          <h2 className='text-xl font-bold mb-2 text-center'>
+        <div key={league.id} className='mb-8 bg-black'>
+          <h2 className='text-xl font-bold mb-2 text-center pt-2'>
             {`${league.country} - ${league.name} (${league.season})`}
           </h2>
           <table className='min-w-full border border-gray-300'>
@@ -25,24 +20,15 @@ const CountryTable: React.FC<CountryTableProps> = ({ footballData }) => {
               <tr className='bg-gray-100'>
                 <th className='p-3 text-center text-white bg-black'>Rank</th>
                 <th className='p-3 text-center text-white bg-black'>Logo</th>
-                <th className='p-3 text-center text-white bg-black'>
-                  Team Name
-                </th>
+                <th className='p-3 text-center text-white bg-black'>Team</th>
                 <th className='p-3 text-center text-white bg-black'>Points</th>
-                <th className='p-3 text-center text-white bg-black'>Form</th>
                 <th className='p-3 text-center text-white bg-black'>Played</th>
                 <th className='p-3 text-center text-white bg-black'>Win</th>
                 <th className='p-3 text-center text-white bg-black'>Draw</th>
                 <th className='p-3 text-center text-white bg-black'>Lose</th>
-                <th className='p-3 text-center text-white bg-black'>
-                  Goals For
-                </th>
-                <th className='p-3 text-center text-white bg-black'>
-                  Goals Against
-                </th>
-                <th className='p-3 text-center text-white bg-black'>
-                  Goals Difference
-                </th>
+                <th className='p-3 text-center text-white bg-black'>G</th>
+                <th className='p-3 text-center text-white bg-black'>GD</th>
+                <th className='p-3 text-center text-white bg-black'>Form</th>
               </tr>
             </thead>
             <tbody>
@@ -53,8 +39,8 @@ const CountryTable: React.FC<CountryTableProps> = ({ footballData }) => {
                       key={standing.rank}
                       className='border-t border-gray-300'
                     >
-                      <td className='p-3 text-center'>{standing.rank}</td>
-                      <td className='p-3 text-center'>
+                      <td className='p-2 text-center'>{standing.rank}</td>
+                      <td className='p-2 text-center'>
                         {standing.team.logo && (
                           <Image
                             src={standing.team.logo}
@@ -64,42 +50,41 @@ const CountryTable: React.FC<CountryTableProps> = ({ footballData }) => {
                           />
                         )}
                       </td>
-                      <td className='p-3 text-center'>{standing.team.name}</td>
-                      <td className='p-3 text-center'>{standing.points}</td>
-                      <td className='p-3 text-center'>
-                        {standing?.form?.split('').map((letter, index) => (
-                          <span
-                            key={index}
-                            className={`${
-                              letter === 'W'
-                                ? 'text-green-500'
-                                : letter === 'D'
-                                ? 'text-yellow-600'
-                                : 'text-red-500'
-                            }`}
-                          >
-                            {letter}
-                          </span>
-                        ))}
-                      </td>
-                      <td className='p-3 text-center'>{standing.all.played}</td>
-                      <td className='p-3 text-center'>{standing.all.win}</td>
-                      <td className='p-3 text-center'>{standing.all.draw}</td>
-                      <td className='p-3 text-center'>{standing.all.lose}</td>
-                      <td className='p-3 text-center'>
-                        {standing.all.goals.for}
-                      </td>
-                      <td className='p-3 text-center'>
-                        {standing.all.goals.against}
+                      <td className='p-2 text-center'>{standing.team.name}</td>
+                      <td className='p-2 text-center'>{standing.points}</td>
+                      <td className='p-2 text-center'>{standing.all.played}</td>
+                      <td className='p-2 text-center'>{standing.all.win}</td>
+                      <td className='p-2 text-center'>{standing.all.draw}</td>
+                      <td className='p-2 text-center'>{standing.all.lose}</td>
+                      <td className='p-2 text-center'>
+                        {standing.all.goals.for}:{standing.all.goals.against}
                       </td>
                       <td
-                        className={`p-3 text-center ${
+                        className={`p-2 text-center ${
                           standing.goalsDiff > 0
                             ? 'text-green-500'
                             : 'text-red-500'
                         }`}
                       >
                         {standing.goalsDiff}
+                      </td>
+                      <td className='p-2 text-center'>
+                        <div className='flex'>
+                          {standing?.form?.split('').map((letter, index) => (
+                            <div
+                              key={index}
+                              className={`${
+                                letter === 'W'
+                                  ? 'bg-green-500'
+                                  : letter === 'D'
+                                  ? 'bg-yellow-500'
+                                  : 'bg-red-500'
+                              } text-center rounded-full w-6 h-6 text-sm flex items-center justify-center mx-1`}
+                            >
+                              {letter}
+                            </div>
+                          ))}
+                        </div>
                       </td>
                     </tr>
                   )),
